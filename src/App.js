98ae21f27f3fe59618a20ed24e6906d143b10fd5
Edg09Lopez <edgeduardolopez90@gmail.com>
2022-06-19 17:02:01 -0300

    pags

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Noticia from './components/Noticia';
 import Header from './components/Header';
 import ClassComponent from './components/ClassComponent';
 import Counter from './components/Counter';
-
+import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      {/*<Header></Header>
       <Noticia titulo="Primera Noticia" descripcion="Detalle de la primera noticia"></Noticia>
       <ClassComponent titulo="Class Component"></ClassComponent>
-      <Counter></Counter>
+      <Counter></Counter>*/}
+      <Home></Home>
     </div>
   );
 }
